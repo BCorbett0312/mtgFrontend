17a43f5a36c5ff64000ae502621a5ce7ae6df10b
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-08-05 18:36:41 -0400

    trying to correct issue with dependency

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-express = require("express");
-path = require("path");
+const express = require("express");
+const path = require("path");
 
-app = express();
+const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/theCardPool"));
+app.use(express.static(__dirname + "/dist/mtgfrontend"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join("./dist/theCardPool/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/mtgfrontend/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
