73fcb8f10e075ee3eadd075188629c24683bba6a
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-08-06 16:57:40 -0400

    Updated Server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/mtgFrontend'));
+app.use(express.static('/dist/mtgFrontend'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/mtgFrontend/index.html'));
+  res.sendFile(path.join('/dist/mtgFrontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
