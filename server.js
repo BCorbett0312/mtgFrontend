36d43434990587b988a1ab10fb418fc37a8bec46
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-08-06 15:52:04 -0400

    Updated Server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/mtgfrontend"));
+app.use(express.static("./dist/mtgfrontend"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join(__dirname + "/dist/mtgfrontend/index.html"));
+  res.sendFile(path.join("./dist/mtgfrontend/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
