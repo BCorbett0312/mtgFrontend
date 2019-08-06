3c867c6edd8e52f4c415109918136d1892d66f96
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-08-06 16:01:25 -0400

    Updated Server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("./dist/mtgaccumulatedknowledge"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join("./dist/mtgaccumulatedknowledge/index.html"));
+  res.sendFile(path.join(__dirname, "./dist/mtgaccumulatedknowledge/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
