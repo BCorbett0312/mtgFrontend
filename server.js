6f2a1294237f41e873faaefb231a957c673b1c41
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-08-06 15:59:32 -0400

    Updated Server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/theCardPool"));
+app.use(express.static("./dist/mtgaccumulatedknowledge"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join("./dist/theCardPool/index.html"));
+  res.sendFile(path.join("./dist/mtgaccumulatedknowledge/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
